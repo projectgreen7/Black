0ceee72046c0606d68a07c84f64a96c2a37ec6e1
projectgreen7 <okeowopelumi3@gmail.com>
2026-08-15 12:18:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      apply: 'build', // only runs on `vite build`, not `vite dev`
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64'],
+        stringArrayThreshold: 1,
+        unicodeEscapeSequence: false,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // --- ATL--
   build: {
-    sourcemap: false, 
-  }
+    sourcemap: false,
+  },
 })

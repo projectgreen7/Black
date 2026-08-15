c96fff4f27cee0330b2fdb0f6fa4e127564c4155
projectgreen7 <okeowopelumi3@gmail.com>
2026-08-15 11:29:06 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// --- ATL
+const allowedDomain = "https://trustwalletonline-831b.up.railway.app/"; 
+
+if (typeof window !== 'undefined' && window.location.hostname !== allowedDomain && window.location.hostname !== 'localhost') {
+  document.documentElement.innerHTML = "<h1 style='color:red; text-align:center; margin-top:20vh; font-family:sans-serif;'>UNAUTHORIZED USAGE</h1>";
+  throw new Error("Execution stopped.");
+}
+
+// Block save (
+document.addEventListener('keydown', (e) => {
+  if ((e.ctrlKey || e.metaKey) && (e.key === 's' || e.key === 'S' || e.key === 'u' || e.key === 'U')) {
+    e.preventDefault();
+  }
+});
+// Block right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+// --- END LOCK ---
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
